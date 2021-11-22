a596292cbb1f5cae7fe9207fb9c898673488f657
abhibongale <abhishekbongale@outlook.com>
2021-11-22 19:34:15 +0000

    added src's

--- src/preprocess.js
+++ src/preprocess.js
@@ -1,9 +1,62 @@
 // preprocess class 
 class Preprocess {
-    constructor(data, labels) {
+    constructor() {
+        this.trainingData = [];
+        this.trainingLabels = [];
+
+        this.testData = [];
+        this.testLabel = [];
+        
         this.doodles = [];
+        
         this.labelData = [];
-        this.returnTensors = [];
-        this.d = data;
+        this.tensorData = [];
+
+        this.totalPixels = 784;
+        this.doodleLabels = ["flower", "cat", "bird"];
+    }
+
+    prepareData(data, labels) {
+        console.log("converting the data into tensors");
+
+        for (let row = 0; row < this.data.length; row++) {
+            let pixels = []; // store the image pixels.
+            for (let col = 0; col <  totalPixels; col++) {
+                pixels[col] = data[row][col] /255;
+            }
+            this.doodles[i] = pixels; // store the all images pixels.
+            let doodleIdx = doodleLabels.indexOf(labels[row]);
+            this.labelData[i] = doodleIdx;
+        }
+        x = tf.tensor2d(doodles); // Creates rank-2 tf.Tensor
+        let labelTensor = tf.tensor1d(this.labelData, "int32"); // Creates rank-1 tf.Tensor.
+        y = tf.oneHot(labelTensor, this.doodleLabels.length).cast("float32"); // Creates a one-hot tf.Tensor (one-hot encoding)
+        labelTensor.dispose(); // Disposes tf.Tensor from memory.
+        // store the tensors of data and labels as (labels, data)
+        this.tensorData[0] = x;
+        this.tensorData[1] = y;
+
+        return this.tensorData;
+    }
+
+    initializeData() {
+        console.log("Initialising data.........");
+        for (let i = 0; i < this.doodleLabels.length; i++) {
+            let label = this.doodleLabels[i];
+            // create data object array
+            console.log("started with: " + label);
+            dataObject[i] = new Object(label);
+            dataObject[i].loadBytesData();
+            dataObject[i].splitData();
+        }
+
+        for (let i = 0; i < dataObject.length; i++) {
+            this.trainingData = this.trainingData.concat(dataObject[i].trainingData);
+            this.trainingLabels = this.trainingLabels.concat(dataObject[i].trainingLabels);
+            this.testingData = this.testingData.concat(dataObject[i].trainingData);
+            this.testingLabels = this.testingLabels.concat(dataObject[i].testingLabels);
+            console.log(this.trainingData, this.trainingLabels);
+        }
+        console.log("done");
     }
 }
--- src/sketch.js
+++ src/sketch.js
@@ -1,12 +1,151 @@
-// contains all the data objects
+// contain all the object array
+let dataObjects = [];
 
-let dataObjectsArray = [];
-
-
-// contains the arrays of preloaded data
+// contains the arrays of preload data
 let dataPreload = [];
 
-// Model
-let model;
+let model = new Model();
+let preprocess = new Preprocess();
+
+x = {};
+y = {};
+testingX = {};
+testingY = {};
+
+const numDoodle = 1000;
+const dataProportion = 0.8;
 
 
+function preload() {
+    console.log("preloading data");
+    let filename;
+    for (let i = 0; i < preprocess.doodleLabel.length; i++) {
+        filename = preprocess.doodleLabel[i].toLowerCase();
+        dataPreload[i] = loadBytes("./datasets/" + filename + ".bin");
+    }
+    console.log(dataPreload);
+    console.log("Done");
+}
+
+// Setup function of p5.js (called after preload)
+async function setup() {
+
+    let canvas = createCanvas(280, 280);
+    canvas.parent("canvas");
+    background(0);
+    preprocess.initializeData();
+
+    console.log("creating training tensors.........");
+    let trainingData = preprocess.prepareData(preprocess.trainingData, preprocess.trainingLabels);
+    xTrain = trainingData[0];
+    yTrain = trainingData[1];
+    console.log("Done");
+    
+    console.log("creating testing tensors.........");
+    let testingData = preprocess.prepareData(preprocess.testingData, preprocess.testingLabels);
+    xTest = testingData[0];
+    yTest = testingData[1];
+    console.log("Done");
+
+    console.log("Creating Model.........");
+    modelBuild = model.buildModel();
+    console.log("Done");
+    
+    // training button
+    let trainButton = select("#train");
+    let trainingAlert = document.getElementById("training-alert");
+    trainingAlert.style.display = "none";
+    trainButton.mousePressed(() => {
+        trainingAlert.style.display = "inline";
+        console.log("Training model");
+
+        // Let's train the model (this .then(() => thingy is an application of the
+        // new ES6 functionnality combined with the js promises).
+        trainingAlert(trainingAlert).then(async() => {
+            // Log progress
+            console.log("Done");
+            trainingAlert.style.display = "none";
+            trainingAlert.className = "btn btn-disabled";
+        });
+    });
+
+    // guess button
+    let guessButton = select("#guess");
+    guessButton.mousePressed(function() {
+        let inputs = [];
+        let inputImage = [];
+        let img = get();
+        img.resize(28, 28);
+        img.loadPixels();
+        // Convert black in white drawings to white in black drawings 
+        for (let i = 0; i < preprocess.totalPixels; i++) {
+            let alpha = img.pixels[i * 4];
+            // normalize the pixels
+            inputs[i] = alpha / 255.0;
+        }
+        
+        // create 2D array with this pixels
+        inputImage[0] = inputs;
+
+        // Convert array to tensor
+        let tensorTOPredict = tf.tensor2d(inputImage);
+        console.log(tensorToPredict);
+
+        // predict the doodle
+        let guess = model.predict(tensorToPredict);
+        let argMax = guess.argMax(1);
+        let classifiedLabel = argMax.dataSync()[0];
+
+        let classifiedDoodleLabel = preprocess.doodleLabel[classifiedLabel];
+        const ouput = select("#ouput");
+        output.html(classifiedDoodleLabel + "!!!");
+        console.log("Guessed: " + classifiedDoodleLabel);
+    });
+
+    // clear button
+    let clearButton = select("#clear");
+    clearButton.mousePressed(function() {
+        background(0);
+    });
+
+    let generateButton = select("#generate");
+
+    generateButton.mousePressed(function(){
+        background(0);
+        let randomIndex = floor(random(numDoodle * reprocess.doodleLabel.length * (1 - dataProportion)));
+        // testing X
+        let offset = randomIndex * preprocess.totalPixels;
+        let doodlePixels = testingX.dataSync().subarray(offset, offset + preprocess.totalPixels);
+        // testing y
+        let otherOffset = randomIndex * preprocess.doodleLabels.length;
+        let labelResult = testingY.dataSync().subarray(offset, offset + preprocess.totalPixels);
+
+        let doodleIndex;
+        for (let i = 0; i < array.length; i++) {
+            if (labelResult[i] === 1) {
+                doodleIndex = i;
+            }
+        }
+        console.log(preprocess.doodleLabels[doodleIndex]);
+        let img = createImage(28, 28);
+        img.loadPixels();
+        for (let i = 0; i < preprocess.totalPixels; i++) {
+            let val = doodlePixels[i] * 255;
+            img.pixels[i * 4 + 0] = val;
+            img.pixels[i * 4 + 1] = val;
+            img.pixels[i * 4 + 2] = val;
+            img.pixels[i * 4 + 3] = 255;
+        }
+        img.updatePixels();
+        img.resize(280, 280);
+        image(img, 0, 0);
+    });
+}
+
+function draw() {
+    strokeWeight(8);
+    stroke(255);
+    if (mouseIsPressed) {
+      line(pmouseX, pmouseY, mouseX, mouseY);
+    }
+  }
