2e073929b2de75fd7221678a3a9358f67b5cd002
abhibongale <abhishekbongale@outlook.com>
2021-11-30 00:27:04 +0000

    working doodle_object, doodle_preprocess

---
+++ src/doodle_object.js
@@ -0,0 +1,81 @@
+class DataObject {
+    constructor(label, doodleLabelList, dataPreload) {
+        this.label = label;
+        this.totalData = [];
+        this.trainingData = [];
+        this.testingData = [];
+        this.trainingLabels = [];
+        this.testingLabels = [];
+        this.dataPreload = dataPreload;
+        this.PROPORTION = 0.8;
+        this.NUMDOODLE = 1000;
+        this.BYTELEN = 784;
+        this.NUMEACHDOODLE = 1000;
+        this.doodleLabelList = doodleLabelList;
+        console.log(this.label);
+        console.log(this.doodleLabelList);
+        
+    }
+
+    get trainingData() {
+        return this._trainingData;
+      }
+
+      get testingData() {
+        return this._testingData;
+      }
+
+      get trainingLabels() {
+        return this._trainingLabels;
+      }
+      
+      get testingLabels() {
+        return this._testingLabels;
+      }
+    
+      set trainingData(data) {
+        this._trainingData = data;
+      }
+    
+      set testingData(data) {
+        this._testingData = data;
+      }
+    
+      set trainingLabels(data) {
+        this._trainingLabels = data;
+      }
+    
+      set testingLabels(data) {
+        this._testingLabels = data;
+      }
+    
+
+    loadBytesData() {
+        console.log("loadBytesData");
+        let index = this.doodleLabelList.indexOf(this.label);
+        //console.log("index: " +  index);
+        let bytesObject = this.dataPreload[index];
+        //console.log("this.dataPreload[index]: " +  this.dataPreload[index]);
+        this.totalData = bytesObject.bytes; // bytesObject.bytes is the actual bytes array
+    }
+
+    splitData() {
+        console.log("splitData");
+        for (let i = 0; i < this.NUMDOODLE; i++) {
+            let offset = i * this.BYTELEN; // keeping track of index
+            //console.log("offset: " + offset);
+            let threshold = floor(this.PROPORTION * this.NUMDOODLE);
+            //console.log("threshold: " + threshold);
+            if (i < threshold) {
+                this.trainingData[i] = this.totalData.subarray(offset, offset + this.BYTELEN);
+                this.trainingLabels[i] = this.label;
+            } else {
+                this.testingData[i - threshold] = this.totalData.subarray(offset, offset + this.BYTELEN);
+                this.testingLabels[i - threshold] = this.label;
+            }
+        }
+    }
+
+
+}
+
---
+++ src/doodle_preprocess.js
@@ -0,0 +1,60 @@
+class Preprocess {
+    constructor(DOODLELABELLIST, dataPreload) {
+        
+        // train split
+        this.trainingData = [];
+        this.trainingLabels = [];
+        // test split
+        this.testingData = [];
+        this.testingLabels = [];
+
+        this.DATALENGTH = 784;
+        this.DOODLELABELLIST = DOODLELABELLIST;
+        this.dataPreload = dataPreload;
+       // console.log("dataPreload " + this.dataPreload);
+       // console.log("DOODLELABELLIST " + this.DOODLELABELLIST);
+    }
+
+    prepareData(data, labels) {
+        console.log("prepare data");
+        let doodles = [];
+        let labelsData = [];
+        let returnedTensors = []; 
+
+        for (let row = 0; row < data.length; row++) {
+            let pixels = [];
+            for (let col = 0; col < this.DATALENGTH; col++) {
+                pixels[col] = data[row][col] / 255;
+            }
+            doodles[row] = pixels;
+            let doodleIndex = this.DOODLELABELLIST.indexOf(labels[row]);
+            labelsData[row] = doodleIndex;   
+        }
+        x = tf.tensor2d(doodles);
+        let labelsTensor = tf.tensor1d(labelsData, "int32");
+        y = tf.oneHot(labelsTensor, this.DOODLELABELLIST.length).cast("float32");
+        labelsTensor.dispose();
+        returnedTensors[0] = x;
+        returnedTensors[1] = y;
+        return returnedTensors;
+    }
+
+    initializeData() {
+        console.log("INITIALIZE DATA");
+        for (let i = 0; i < this.DOODLELABELLIST.length; i++) {
+            let label = this.DOODLELABELLIST[i];
+            dataObjectsArray[i] = new DataObject(label, this.DOODLELABELLIST, this.dataPreload);
+            dataObjectsArray[i].loadBytesData();
+            dataObjectsArray[i].splitData();
+        }
+
+        for (let i = 0; i < dataObjectsArray.length; i++) {
+            this.trainingLabels = this.trainingLabels.concat(dataObjectsArray[i].trainingLabels);
+            this.testingLabels  = this.testingLabels.concat(dataObjectsArray[i].testingLabels);
+            this.trainingData   = this.trainingData.concat(dataObjectsArray[i].trainingData);
+            this.testingData    = this.testingData.concat(dataObjectsArray[i].testingData);
+            console.log(this.trainingData, this.trainingLabels);
+        };
+        console.log("Done");
+    }
+}
